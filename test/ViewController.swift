8223ed503fe8481efb5a8820d0f2b9af37247d74
Balladorim <Alexander@Mac-mini-Alexander.local>
2016-05-11 18:24:24 +0800

    Initial commit from local git

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Alexander Kostyuk on 11.05.16.
+//  Copyright © 2016 Alexander Kostyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
